115fb748df599d03e638d13e0c89d019d3e9b461
kj84park <netom@naver.com>
2018-05-05 22:37:13 +0900

    change .gitignore

---
+++ VideoExample/VideoExample/MainViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewCell.swift
+//  VideoExample
+//
+//  Created by mac on 2018. 5. 5..
+//  Copyright © 2018년 StudioKJ. All rights reserved.
+//
+
+import UIKit
+
+class MainViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ VideoExample/VideoExample/VideoPlayerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  VideoPlayerViewController.swift
+//  VideoExample
+//
+//  Created by mac on 2018. 5. 5..
+//  Copyright © 2018년 StudioKJ. All rights reserved.
+//
+
+import UIKit
+
+class VideoPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
